1330b0f156e2275acd1a9256fc13a1d22e5e3a9e
James Nguyen <james@jojojames.com>
2026-06-12 02:32:35 -0400

    Clear

--- ibuffer-sidebar.el
+++ ibuffer-sidebar.el
@@ -25,12 +25,6 @@
 
 ;;; Commentary:
 ;; Provides a sidebar interface similar to `dired-sidebar', but for `ibuffer'.
-
-;;
-;; (use-package ibuffer-sidebar
-;;   :bind (("C-x C-b" . ibuffer-sidebar-toggle-sidebar))
-;;   :ensure nil
-;;   :commands (ibuffer-sidebar-toggle-sidebar))
 ;;
 
 ;;; Code:
